a4efca96f339e4daba1006a505240cc03833a568
Ar Monarch <praffulthapa12@gmail.com>
2023-09-27 11:46:20 +0545

    Create a route for introduction to computer science and programmin in python

--- src/app/courses/[courseId]/page.js
+++ src/app/courses/[courseId]/page.js
@@ -4,10 +4,7 @@ export default function course({params}){
     return (
         <div className="text-2xl">
             The Course page {params.courseId} Includes Topics
-            {console.log(params)}
             <ul>
-                <li><Link href={`/courses/${params.courseId}/computer_science`} className="text-blue-600 hover:text-red-500">Computer Science</Link></li>
-                <li><Link href={`/courses/${params.courseId}/IoT`}  className="text-blue-600 hover:text-red-500">IoT</Link></li>
             </ul>
         </div>
     )
--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -12,6 +12,7 @@ export default function courses(){
             <ul>
                 <li><Link href="/courses/Introduction_to_Computer_Science" className="text-blue-600 hover:text-red-500">Introduction to Computer Science</Link></li>
                 <li><Link href="/courses/Introduction_to_IoT" className="text-blue-600 hover:text-red-500">Introduction to IoT</Link></li>
+                <li><Link href="/courses/introduction_to_computer_science_and_programming_in_python" className="text-blue-600 hover:text-red-500">Introduction to Computer Science and Programming in Python</Link></li>
             </ul>
         </div>
     )
