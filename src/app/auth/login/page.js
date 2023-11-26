8749e8cd12173927a939e489aa69285ca50ee265
Ardemon <praffulthapa12@gmail.com>
2023-11-26 08:56:40 +0545

    @next/Image to @next/legacy/image for login and register page

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/legacy/image"
 import Link from "next/link"
 import LoginForm from "@/components/LoginForm"
 import { Fira_Code } from "next/font/google"
