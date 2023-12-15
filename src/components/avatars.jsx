b1e95e3c48218387277c16e45318899255ecbc6b
wry420 <nishchalns10@gmail.com>
2023-12-15 20:49:42 +0545

    achievements scrollable area added

---
+++ src/components/avatars.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Peep from "react-peeps";
+
+const styles = {
+  peepStyle: {
+    width: 300,
+    height: 300,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  circleStyle: {
+    backgroundColor: "#F3D34A",
+    width: 270,
+    height: 270,
+    alignSelf: "center",
+    borderRadius: 135,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: "black",
+    borderStyle: "solid",
+  },
+  showcaseWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    height: "-webkit-fill-available",
+  },
+};
+
+export function Avatar() {
+  return <div></div>;
+}
